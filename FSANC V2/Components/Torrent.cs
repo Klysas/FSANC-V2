ed9ca7eca585c57509ef79a4564fa99692255ac1
Klysas <martynasvilkas510@gmail.com>
2015-09-30 15:52:37 +0300

    ADD: FileaRenamer and Torrent.

---
+++ FSANC V2/Components/Torrent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FSANC_V2.Components
+{
+	public partial class Torrent : UserControl
+	{
+		#region Public constructors
+
+		public Torrent()
+		{
+			InitializeComponent();
+		}
+
+		#endregion
+
+		#region Public methods
+
+		public void Update(AbstractVideo video)
+		{
+
+			switch (video.Type)
+			{
+				case AbstractVideo.VideoType.MOVIE: Update(video as Movie);
+					break;
+				case AbstractVideo.VideoType.SERIES: Update(video as Series);
+					break;
+			}
+			this.Update();
+		}
+
+		#endregion
+
+		#region Private methods
+
+		private void Update(Movie movie)
+		{
+
+		}
+
+		private void Update(Series series)
+		{
+
+		}
+
+		#endregion
+	}
+}
